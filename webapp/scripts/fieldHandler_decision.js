e3298eceffbbfcb09ba69eb4eedac081e0563724
netblue4 <30316178+netblue4@users.noreply.github.com>
2025-08-01 13:11:15 +0200

    Update fieldHandler_decision.js

--- webapp/scripts/fieldHandler_decision.js
+++ webapp/scripts/fieldHandler_decision.js
@@ -6,11 +6,15 @@ function createDecisionField(field, capturedData) {
     const decisions = decisionsString.split('/');
     
     const capturedData_field_name_to_interogate = field.FieldName;
-    const decision_Yes = field.YesTarget;//decisions[0].split('=')[1];
-    const decision_No = field.NoTarget;//decisions[1].split('=')[1];
+    const target_Yes = field.YesTarget;//decisions[0].split('=')[1];
+    const target_No = field.NoTarget;//decisions[1].split('=')[1];
     const selectedValue = capturedData[capturedData_field_name_to_interogate]
 
-    window.getStepKeyByString(selectedValue);
+    if (selectedValue === "Yes") {
+        window.getStepKeyByString(target_Yes);
+    } else {
+        window.getStepKeyByString(target_No);
+    }
 
     return fieldDiv;
 }
