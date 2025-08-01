5f32787964b84010e6b5f283e1bb3d6e2b12e1b1
netblue4 <30316178+netblue4@users.noreply.github.com>
2025-08-01 12:56:04 +0200

    Update fieldHandler_decision.js

--- webapp/scripts/fieldHandler_decision.js
+++ webapp/scripts/fieldHandler_decision.js
@@ -2,13 +2,13 @@ function createDecisionField(field, capturedData) {
     const fieldDiv = document.createElement('div');
     fieldDiv.className = 'form-field';
 
-    const optionsString = field.FieldType.split(':')[1]?.trim() || '';
-    const options = optionsString.split('/');
+    const decisionsString = field.FieldType.split(':')[1]?.trim() || '';
+    const decisions = decisionsString.split('/');
     
-    const options_CapturedData_field_name_to_interogate = field.FieldName;
-    const options_Yes = options[0].split('=')[1];
-    const options_No = options[1].split('=')[1];
-    const selectedValue = capturedData[options_CapturedData_field_name_to_interogate]
+    const capturedData_field_name_to_interogate = field.FieldName;
+    const decision_Yes = options[0].split('=')[1];
+    const decision_No = options[1].split('=')[1];
+    const selectedValue = capturedData[capturedData_field_name_to_interogate]
 
     window.getStepKeyByString(selectedValue);
 
