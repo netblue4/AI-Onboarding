b76ccb0d01ad4f591fefb516b5d9f3487b9c925f
netblue4 <30316178+netblue4@users.noreply.github.com>
2025-10-09 10:14:39 +0200

    Update fieldHandler_objective.js

--- webapp/scripts/fieldHandler_objective.js
+++ webapp/scripts/fieldHandler_objective.js
@@ -1,1 +1,15 @@
+function createObjective(objective) {
+    const fieldDiv = document.createElement('div');
+    fieldDiv.className = 'form-field';
 
+    const label = document.createElement('label');
+    label.textContent = objective;
+    const span = document.createElement('span');
+    span.className = 'auto-generated-label';
+
+    span.textContent = objective;
+    fieldDiv.appendChild(label);
+    fieldDiv.appendChild(span);
+
+    return fieldDiv;
+}
