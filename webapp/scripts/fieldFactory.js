9b60dc5c21444dbe4315c88db65cb7550cba9c3f
netblue4 <netblue4@googlemail.com>
2025-10-25 08:51:44 +0200

    Added a new field group fieldType

--- webapp/scripts/fieldFactory.js
+++ webapp/scripts/fieldFactory.js
@@ -14,6 +14,7 @@ function getFieldHandler(fieldType) {
         'risk':createRisk,
         'objective':createObjective,
         'plan':createPlan,
+        'fieldGroup':createFieldGroup,
     };
     
     if (exactMatchHandlers[fieldType]) {
